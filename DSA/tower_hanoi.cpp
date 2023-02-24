1f41ce42b725991cf93283e711f8be1c71cfe34e
MDHASIBASKARI <mdhsbaskari@gmail.com>
2023-02-25 00:14:40 +0600

    Tower of Hanoi :)

---
+++ DSA/tower_hanoi.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void tower(int n, char A, char B, char C) {
+    if (n == 0) return;
+
+    tower(n-1, A, C, B);                 // A -> B
+    cout << "Move " << A << " to " << C << endl;  // A -> C
+    tower(n-1, B, A, C);                 // B -> C
+}
+
+int main() {
+    tower(2, 'A', 'B', 'C');
+}
